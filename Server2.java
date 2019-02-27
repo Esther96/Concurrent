10a759b81bce683f22358a4829d0a79c306295db
Esther96 <m_estera96@yahoo.com>
2019-02-27 20:59:48 +0200

    Update Server2.java

--- Server2.java
+++ Server2.java
@@ -1,41 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-public class MyServerSocket {
-    private ServerSocket server;
-    public MyServerSocket(String ipAddress) throws Exception {
-        if (ipAddress != null && !ipAddress.isEmpty()) 
-          this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
-        else 
-          this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
-    }
-    private void listen() throws Exception {
-        String data = null;
-        Socket client = this.server.accept();
-        String clientAddress = client.getInetAddress().getHostAddress();
-        System.out.println("\r\nNew connection from " + clientAddress);
-        
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(client.getInputStream()));        
-        while ( (data = in.readLine()) != null ) {
-            System.out.println("\r\nMessage from " + clientAddress + ": " + data);
-        }
-    }
-    public InetAddress getSocketAddress() {
-        return this.server.getInetAddress();
-    }
-    
-    public int getPort() {
-        return this.server.getLocalPort();
-    }
-    public static void main(String[] args) throws Exception {
-        MyServerSocket app = new MyServerSocket(args[0]);
-        System.out.println("\r\nRunning Server: " + 
-                "Host=" + app.getSocketAddress().getHostAddress() + 
-                " Port=" + app.getPort());
-        
-        app.listen();
-    }
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+ public static void main(String argv[]) throws Exception {
+  String clientSentence;
+  String capitalizedSentence;
+  ServerSocket welcomeSocket = new ServerSocket(6789);
+
+  while (true) {
+   Socket connectionSocket = welcomeSocket.accept();
+   BufferedReader inFromClient =
+    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+   clientSentence = inFromClient.readLine();
+   System.out.println("Received: " + clientSentence);
+   capitalizedSentence = clientSentence.toUpperCase() + 'n';
+   outToClient.writeBytes(capitalizedSentence);
+  }
+ }
 }
