2cc66b5f31bd050cd6de5862f44be862f6d91034
Esther96 <m_estera96@yahoo.com>
2019-02-27 21:31:09 +0200

    Update Client2.java

--- Client2.java
+++ Client2.java
@@ -1,18 +1,46 @@
-import java.io.*;
-import java.net.*;
-
-class TCPClient {
- public static void main(String argv[]) throws Exception {
-  String sentence;
-  String modifiedSentence;
-  BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-  Socket clientSocket = new Socket("localhost", 6789);
-  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-  sentence = inFromUser.readLine();
-  outToServer.writeBytes(sentence + 'n');
-  modifiedSentence = inFromServer.readLine();
-  System.out.println("FROM SERVER: " + modifiedSentence);
-  clientSocket.close();
- }
-}
+// Java program to illustrate Client Side Programming 
+// for Simple Calculator using TCP 
+import java.io.DataInputStream; 
+import java.io.DataOutputStream; 
+import java.io.IOException; 
+import java.net.InetAddress; 
+import java.net.Socket; 
+import java.net.UnknownHostException; 
+import java.util.Scanner; 
+  
+public class Client 
+{ 
+    public static void main(String[] args) throws IOException 
+    { 
+        InetAddress ip = InetAddress.getLocalHost(); 
+        int port = 4444; 
+        Scanner sc = new Scanner(System.in); 
+  
+        // Step 1: Open the socket connection. 
+        Socket s = new Socket(ip, port); 
+  
+        // Step 2: Communication-get the input and output stream 
+        DataInputStream dis = new DataInputStream(s.getInputStream()); 
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+  
+        while (true) 
+        { 
+            // Enter the equation in the form- 
+            // "operand1 operation operand2" 
+            System.out.print("Enter the equation in the form: "); 
+            System.out.println("'operand operator operand'"); 
+  
+            String inp = sc.nextLine(); 
+  
+            if (inp.equals("bye")) 
+                break; 
+  
+            // send the equation to server 
+            dos.writeUTF(inp); 
+  
+            // wait till request is processed and sent back to client 
+            String ans = dis.readUTF(); 
+            System.out.println("Answer=" + ans); 
+        } 
+    } 
+} 
